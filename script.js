86c8fb2f742bbf1b52d8e18dd128d9fa42adc123
desireekodehode <desireekodehode1@gmail.com>
2023-09-14 22:05:17 +0200

    *

---
+++ script.js
@@ -0,0 +1,19 @@
+// LOOPS
+
+const names = [
+    "Victoria",
+    "Kristine",
+    "Dagny",
+    "Sofia",
+    "Dorris",
+    "Sara",
+    "Petra",
+    "Susen",
+    "Bodil",
+    "Anne"
+];
+
+for (let i = 0; i < names.length; i++) {
+    console.log(`Heisann ${names[i]}`);
+}
+
